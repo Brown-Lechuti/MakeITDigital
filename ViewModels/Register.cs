79541f5cf6aee60f10d7df5095e05d1a4215ed95
BrownLechuti <brownlechuti@gmail.com>
2021-11-10 21:14:53 +0200

    Create and implement registration page with ms identity

---
+++ ViewModels/Register.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MakeITDigital.ViewModels
+{
+    public class Register
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage ="Confirm password did not match password")]
+        public string confirmPassword { get; set; }
+
+    }
+}
